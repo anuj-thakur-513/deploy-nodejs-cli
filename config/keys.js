0c39a1a2327e05ffed9f4949581eed2826423838
anuj-thakur-513 <anuj.thakur@mathongo.com>
2024-11-04 16:59:21 +0530

    add keys for AWS && utility function

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    aws: {
+        ami: process.env.AWS_AMI_ID,
+        securityGroup: process.env.AWS_SECURITY_GROUP_ID,
+        iamRole: process.env.AWS_IAM_ROLE_NAME,
+    },
+};
---
+++ utils/extractProjectName.js
@@ -0,0 +1,11 @@
+const extractProjectName = (repoUrl) => {
+    // Extract the last part of the URL after the last '/'
+    const lastSegment = repoUrl.split("/").pop();
+
+    // Remove the .git suffix if it exists
+    const directoryName = lastSegment.endsWith(".git") ? lastSegment.slice(0, -4) : lastSegment;
+
+    return directoryName;
+};
+
+module.exports = extractProjectName;
